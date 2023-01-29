7f46825d3b92a4394a98637b0da562538f3f6e83
cartwatson <cartwatson@gmail.com>
2023-01-29 13:24:45 -0700

    removed .pyc

--- web/DansAutoBarn/settings.py
+++ web/DansAutoBarn/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1nkbb)0mx@)^y7k^-ub5^_cevoa!99ur(&3)!gjyv1z_vkbf5n'
+# load key from .env
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,9 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # local apps
-    'temp.apps.TempConfig'
 ]
 
 MIDDLEWARE = [
@@ -119,10 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- web/temp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from temp.views import HomeView, CustomerView
-
-app_name = 'temp'
-urlpatterns = [
-    path('', HomeView.as_view(), name='homepage'),
-    path('customer', CustomerView.as_view(), name='customer')
-]
