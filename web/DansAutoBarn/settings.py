1fcba3717185db39686343ada7b6d40495295b9b
cmax <a02335780@usu.edu>
2023-02-06 16:43:15 -0700

    Adjusted view and url files

--- web/DansAutoBarn/settings.py
+++ web/DansAutoBarn/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- web/login/views.py
+++ web/login/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your views here.
+def login(request):
+    return render(request, 'login/index.html')
