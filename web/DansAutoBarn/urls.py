aefb3e2fc20fe6f1ef6962988cce82479f7282af
cartwatson <cartwatson@gmail.com>
2023-02-09 15:12:26 -0700

    updated urls with userauth

--- web/DansAutoBarn/urls.py
+++ web/DansAutoBarn/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('UserAuth.urls')), # TODO: DESIGN DECISION: how do we want to structure URLs
     path('admin/', admin.site.urls),
 ]
