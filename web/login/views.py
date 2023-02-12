2a09fd68fb5086b2f09356538ed0f2b809425cfb
cartwatson <cartwatson@gmail.com>
2023-02-12 14:47:55 -0700

    removed login app

--- web/login/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.utils import timezone
-
-def login(request):
-	return render(request, 'login/login.html')
