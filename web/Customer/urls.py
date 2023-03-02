642eaf13366e0b3023c6405f329fbbc17a6af6c0
cmax <a02335780@usu.edu>
2023-03-01 19:06:52 -0700

    Messed around with Vue Router

--- web/Customer/urls.py
+++ web/Customer/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'Customer'
 urlpatterns = [
-    path('profile/', views.profile, name="profile"),
+    re_path(r'^profile/.*', views.profile, name="profile"),
     path('search/', views.search_for_res, name='search'),
 ]
