c80a1fa9edbd0e161b597463512240fde9704e3c
Caden Maxwell <cadenr.maxwell@gmail.com>
2023-03-03 10:24:03 -0700

    Added some comments

--- web/Customer/urls.py
+++ web/Customer/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'Customer'
 urlpatterns = [
-    re_path(r'^profile/.*', views.profile, name="profile"),
+    re_path(r'^profile/.*', views.profile, name="profile"), # Used regex path to include all paths with the prefix 'profile/'
     path('search/', views.search_for_res, name='search'),
 ]
--- web/DansAutoBarn/urls.py
+++ web/DansAutoBarn/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('UserAuth.urls')),
-    path('', include('Customer.urls')),
     path('', include('django.contrib.auth.urls')),
+    path('', include('Customer.urls')),
 ]
