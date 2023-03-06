8e097e070c9499c22b928567c69ce10be491ef34
Caden Maxwell <cadenr.maxwell@gmail.com>
2023-03-06 13:45:59 -0700

    Added ability to transfer money to account

--- web/Customer/urls.py
+++ web/Customer/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'Customer'
 urlpatterns = [
+    path('profile/balance', views.add_balance, name="balance"),
     re_path(r'^profile/.*', views.profile, name="profile"), # Used regex path to include all paths with the prefix 'profile/'
     path('search/', views.search_for_res, name='search'),
 ]
--- web/Customer/views.py
+++ web/Customer/views.py
@@ -5,6 +5,25 @@ from .models import Car
 def profile(request):
     return render(request, 'Customer/profile.html')
 
+def add_balance(request):
+    if request.method != "POST":
+        return render(request, 'Customer/profile.html')
+    try:
+        amount = int(request.POST.get("inputBal", 0))
+        if amount < 0: raise ValueError
+    except ValueError:
+        context = { "msg": "Amount must be a positive integer" }
+        return render(request, 'Customer/profile.html', context)
+
+    try:
+        request.user.userprofile.balance += amount
+        request.user.userprofile.save()
+        context = { "msg": f"Successfully added ${amount} to account!" }
+    except:
+        context = {"msg": "Something went wrong... Unable to transfer funds."}
+
+    return render(request, 'Customer/profile.html', context)
+
 def search_for_res(request):
     time_now = timezone.now()
     formatedDate = time_now.strftime("%m-%d-%Y")
