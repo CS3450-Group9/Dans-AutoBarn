5d69a5219f800495fe4916e65fe4420125648ca2
Caden Maxwell <cadenr.maxwell@gmail.com>
2023-03-16 00:27:03 -0600

    Password change page working

--- web/Customer/urls.py
+++ web/Customer/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 app_name = 'Customer'
 urlpatterns = [
+    path('profile/pass-change', views.password_change, name="pass_change"),
     path('profile/balance', views.add_balance, name="balance"),
     re_path(r'^profile/.*', views.profile, name="profile"), # Used regex path to include all paths with the prefix 'profile/'
     path('search/', views.search_for_res, name='search'),
--- web/DansAutoBarn/settings.py
+++ web/DansAutoBarn/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,7 @@ LOGOUT_REDIRECT_URL =   '/login'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
