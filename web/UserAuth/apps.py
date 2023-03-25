c9c8ec28277560a32f11d3e129c6538b57ed2ed1
Caden Maxwell <cadenr.maxwell@gmail.com>
2023-03-24 21:41:44 -0600

    Unconfirmed reservations now deleted upon logout

--- web/UserAuth/apps.py
+++ web/UserAuth/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UserauthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'UserAuth'
+
+    def ready(self):
+        import UserAuth.signals
