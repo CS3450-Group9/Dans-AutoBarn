ffc08e3208d90573193b5e7e7478c0ad97857ea0
cmax <a02335780@usu.edu>
2023-04-05 16:21:03 -0600

    Got buttons to appear nicely

--- web/Employee/views.py
+++ web/Employee/views.py
@@ -14,18 +14,24 @@ def staff(request, tab):
     formatted_date = time_now.strftime("%m-%d-%Y")
     context = {"formatted_date": formatted_date}
     tabs = [
-        {"url": "active-rentals",
+        {
+            "url": "active-rentals",
             "tab_title": "Active Rentals",
             "component_name": "ActiveRentals",
-            "template": 'Employee/staffTabs/activeRentals.html' },
-        {"url": "verify",
+            "template": 'Employee/staffTabs/activeRentals.html'
+        },
+        {
+            "url": "verify",
             "tab_title": "Verify Pick-Up",
             "component_name": "Verify",
-            "template": 'Employee/staffTabs/verifyPickup.html' },
-        {"url": "broken-cars",
+            "template": 'Employee/staffTabs/verifyPickup.html'
+        },
+        {
+            "url": "broken-cars",
             "tab_title": "Currently Broken Cars",
             "component_name": "BrokenCars",
-            "template": 'Employee/staffTabs/brokenCars.html' },
+            "template": 'Employee/staffTabs/brokenCars.html'
+        },
     ]
 
     if not request.user.is_authenticated:
@@ -34,45 +40,59 @@ def staff(request, tab):
     elif request.user.userprofile.auth_level == "TW" or request.user.userprofile.auth_level == "CR":
         # For now, CR and TW have the same tabs
         tabs += [
-            {"url": "log-hours",
-             "tab_title": "Log Hours Worked",
-             "component_name": "LogHours",
-             "template": 'Employee/staffTabs/logHours.html' },
+            {
+                "url": "log-hours",
+                "tab_title": "Log Hours Worked",
+                "component_name": "LogHours",
+                "template": 'Employee/staffTabs/logHours.html'
+            },
         ]
         context["tabs"] = tabs
     elif request.user.userprofile.auth_level == "MA":
         tabs += [
-            {"url": "cars",
-             "tab_title": "Manage Cars",
-             "component_name": "CarsView",
-             "template": 'Manager/managerTabs/manageCars.html' },
-            {"url": "users",
-             "tab_title": "Manage Users",
-             "component_name": "UsersView",
-             "template": 'Manager/managerTabs/manageUsers.html' },
-            {"url": "hours",
-             "tab_title": "Review Hours",
-             "component_name": "Hours",
-             "template": 'Manager/managerTabs/reviewHours.html' },
+            {
+                "url": "cars",
+                "tab_title": "Manage Cars",
+                "component_name": "CarsView",
+                "template": 'Manager/managerTabs/manageCars.html'
+            },
+            {
+                "url": "users",
+                "tab_title": "Manage Users",
+                "component_name": "UsersView",
+                "template": 'Manager/managerTabs/manageUsers.html'
+            },
+            {
+                "url": "hours",
+                "tab_title": "Review Hours",
+                "component_name": "HoursView",
+                "template": 'Manager/managerTabs/reviewHours.html'
+            },
         ]
-        buttons = {
-            'CR': [
-                {'text': 'Change to Till Worker', 'value': 'change_to_till_worker'},
-                {'text': 'Demote to Customer', 'value': 'demote_to_customer'},
+        user_buttons = {
+            "CR": [
+                {"text": "Change to Till Worker", "value": "change_to_till_worker"},
+                {"text": "Demote to Customer", "value": "demote_to_customer"},
             ],
-            'TW': [
-                {'text': 'Change to Car Retrieval Specialist', 'value': 'change_to_car_retrieval_specialist'},
-                {'text': 'Demote to Customer', 'value': 'demote_to_customer'},
+            "TW": [
+                {"text": "Change to Car Retrieval Specialist", "value": "change_to_car_retrieval_specialist"},
+                {"text": "Demote to Customer", "value": "demote_to_customer"},
             ],
-            'CU': [
-                {'text': 'Promote to Car Retrieval Specialist', 'value': 'promote_to_car_retrieval_specialist'},
-                {'text': 'Promote to Till Worker', 'value': 'promote_to_till_worker'},
-            ],
+            "CU": [
+                {"text": "Promote to Car Retrieval Specialist", "value": "promote_to_car_retrieval_specialist"},
+                {"text": "Promote to Till Worker", "value": "promote_to_till_worker"},
+            ]
         }
+        users_data = []
+        for user in UserProfile.objects.all():
+            users_data += [{
+                "user_profile": user,
+                "buttons": user_buttons.get(user.auth_level)
+            }]
+
         context["tabs"] = tabs
         context["car_inventory"] = Car.objects.all()
-        context["users"] = UserProfile.objects.all()
-        context["user_buttons"] = buttons
+        context["users_data"] = users_data 
     else:
         return HttpResponseForbidden("Unauthorized: User not part of staff!")
 
